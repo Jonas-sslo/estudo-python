d658ae702b4bd3b8aa8a175429eec42a44c4da8a
Jonas Sanjinez Lopes <batmancarecazz@gmail.com>
2023-05-20 20:05:09 -0300

    Exercicios - Aula 06

--- src/06-arquivos/exercicios/exercicio01.py
+++ src/06-arquivos/exercicios/exercicio01.py
@@ -1,17 +1,6 @@
 """ Exercicio 01 """
 
-
-def carregar_dados_alunos(arq):
-    dados = []
-    for linha in arq.readlines():
-        dados.append(linha.split(','))
-
-    dicionario = []
-    for dado in dados:
-        dicionario.append({'prontuario': dado[0], 'nome': dado[1], 'email': dado[2].strip()})
-
-    return tuple(dicionario)
-
+from dados import carregar_dados_alunos
 
 with open('src/06-arquivos/exercicios/exercicio01-dados.txt', 'r', encoding='UTF-8') as arq:
     print(carregar_dados_alunos(arq))
--- src/06-arquivos/exercicios/exercicio03.py
+++ src/06-arquivos/exercicios/exercicio03.py
@@ -1,19 +1,6 @@
 """ Exercicio 03 """
 
-
-def linha_para_dict(arquivo):
-    dados = []
-    for linha in arquivo:
-        dados.append(linha.split(','))
-
-    lista_chaves = ['prontuario', 'nome', 'email']
-    dicionario = []
-
-    for dado in dados:
-        dicionario.append({lista_chaves[0]: dado[0], lista_chaves[1]: dado[1], lista_chaves[2]: dado[2].strip()})
-
-    return dicionario
-
+from dados import linha_para_dict
 
 with open('src/06-arquivos/exercicios/exercicio01-dados.txt', 'r', encoding='UTF-8') as arquivo:
     for dicionario in linha_para_dict(arquivo):
